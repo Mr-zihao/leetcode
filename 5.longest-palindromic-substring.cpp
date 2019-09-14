bc9ffb0ea6188c793439dd31925d6d5af155faf0
littleeye <upc.zihaolu@gmail.com>
2019-09-14 15:18:08 +0800

    update 5.longest-palindromic-substring.cpp

---
+++ 5.longest-palindromic-substring.cpp
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode id=5 lang=cpp
+ *
+ * [5] Longest Palindromic Substring
+ *
+ * https://leetcode.com/problems/longest-palindromic-substring/description/
+ *
+ * algorithms
+ * Medium (27.88%)
+ * Total Accepted:    650.9K
+ * Total Submissions: 2.3M
+ * Testcase Example:  '"babad"'
+ *
+ * Given a string s, find the longest palindromic substring in s. You may
+ * assume that the maximum length of s is 1000.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "babad"
+ * Output: "bab"
+ * Note: "aba" is also a valid answer.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "cbbd"
+ * Output: "bb"
+ * 
+ * 
+ */
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n=s.size();
+        vector<vector<bool>> dp(n,vector<bool>(n,false));
+        int max_L=0;
+        int start=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j>=0;j--){
+                if(i==j || (i==j+1 && s[i]==s[j]))
+                    dp[j][i]=true;
+                else 
+                    dp[j][i]=(s[j]==s[i]) && dp[j+1][i-1];
+                if(dp[j][i] && (i-j+1)>max_L){
+                    max_L=i-j+1;
+                    start=j;
+                }
+            }
+        }
+        return s.substr(start,max_L);
+        
+    }
+};
